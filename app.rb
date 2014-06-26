9fdb9d68471904c544c8d038db0679e41072bc16
Peter Grunde <peter.a.grunde@gmail.com>
2014-06-26 15:55:33 -0600

    WORK COMPLETE, ZUG ZUG

--- app.rb
+++ app.rb
@@ -1,16 +1,59 @@
+# must type 'rackup' to run this code (layers: Web brick > Rack > Sinatra > My Code)
+# things in initialize are only created on the loading of the server
+
 require "sinatra/base"
-require "sinatra/reloader"
 
 class MyApp < Sinatra::Application
-  register Sinatra::Reloader
 
   def initialize
     super
-    @items = []
+    @items = ["Item 1", "Item 2", "Item 3"]
   end
 
   get "/" do
-    "items length: #{@items.length}"
+    erb :root, :locals => { :items => @items }
+  end
+
+  get "/grumblepugs" do
+    erb :grumblepugs
+  end
+
+  get "/items" do
+    erb :items, :locals => { :items => @items }
+  end
+
+  get "/items/new" do
+    erb :new_item, :locals => { :items => @items }
+  end
+
+  post "/items/form_processing" do
+    data = params[:item_added]
+    @items.push(data)
+    erb :items, :locals => { :items => @items }
+  end
+
+  get "/items/:id" do
+    # this is called 'named parameters' or 'path parameters'
+    identifier = params[:id].to_i
+    "Showing #{@items[identifier]}: <a href='/items/#{identifier}/edit'>Edit Item</a>"
+  end
+
+  get "/items/:id/edit" do
+    identifier = params[:id].to_i
+    erb :item_edit, :locals => { :items => @items, :iden => identifier }
+  end
+
+  post "/items/:id/edit" do
+    identifier = params[:id].to_i
+    data = params[:item_changed]
+    @items[identifier] = data
+    erb :items, :locals => { :items => @items }
+  end
+
+  post "/items/:id/delete" do
+    identifier = params[:id].to_i
+    @items.delete_at(identifier)
+    erb :items, :locals => { :items => @items }
   end
 
   run! if app_file == $0
